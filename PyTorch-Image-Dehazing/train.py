4bc7093bb0385d65c466dc8c20b5fa2a07a94488
Gianna Williams <46410567+GiannaW@users.noreply.github.com>
2021-12-14 01:40:09 -0500

    Update train.py

--- PyTorch-Image-Dehazing/train.py
+++ PyTorch-Image-Dehazing/train.py
@@ -1,104 +1,124 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.backends.cudnn as cudnn
+import torch.optim
 import os
 import sys
-
-import torch
-import torch.utils.data as data
+import argparse
+import net
+import time
+import dataloader
 
 import numpy as np
-from PIL import Image
-import glob
-import random
-import cv2
-
-random.seed(1143)
+from torchvision import transforms
 
 
-def populate_train_list(orig_images_path, hazy_images_path):
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
 
 
-	train_list = []
-	val_list = []
+def train(config):
+
+
+	dehaze_net = net.dehaze_net().cuda()
+	dehaze_net.apply(weights_init)
+
+	# train_dataset = dataloader.dehazing_loader("content/drive/MyDrive/PyTorch-Image-Dehazing/data/data",
+	# 										 "content/drive/MyDrive/PyTorch-Image-Dehazing/data/images")		
+	# val_dataset = dataloader.dehazing_loader("content/drive/MyDrive/PyTorch-Image-Dehazing/data/data",
+	# 										"content/drive/MyDrive/PyTorch-Image-Dehazing/data/images", mode="val")	
+
+ 
+	train_loader = torch.utils.data.DataLoader("content/drive/MyDrive/PyTorch-Image-Dehazing/data/data", batch_size=config.train_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
+	val_loader = torch.utils.data.DataLoader("content/drive/MyDrive/PyTorch-Image-Dehazing/data/images", batch_size=config.val_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
+	print(len(train_loader))
+	criterion = nn.MSELoss().cuda()
+	optimizer = torch.optim.Adam(dehaze_net.parameters(), lr=config.lr, weight_decay=config.weight_decay)
 	
-	image_list_haze = glob.glob(hazy_images_path + "*.jpg")
+	dehaze_net.train()
 
-	tmp_dict = {}
+	for epoch in range(config.num_epochs):
+		for iteration, (img_orig, img_haze) in enumerate(train_loader):
 
-	for image in image_list_haze:
-		image = image.split("/")[-1]
-		key = image.split("_")[0] + "_" + image.split("_")[1] + ".jpg"
-		if key in tmp_dict.keys():
-			tmp_dict[key].append(image)
-		else:
-			tmp_dict[key] = []
-			tmp_dict[key].append(image)
+			img_orig = img_orig.cuda()
+			img_haze = img_haze.cuda()
 
+			clean_image = dehaze_net(img_haze)
 
-	train_keys = []
-	val_keys = []
+			loss = criterion(clean_image, img_orig)
 
-	len_keys = len(tmp_dict.keys())
-	for i in range(len_keys):
-		if i < len_keys*9/10:
-			train_keys.append(list(tmp_dict.keys())[i])
-		else:
-			val_keys.append(list(tmp_dict.keys())[i])
+			optimizer.zero_grad()
+			loss.backward()
+			torch.nn.utils.clip_grad_norm(dehaze_net.parameters(),config.grad_clip_norm)
+			optimizer.step()
 
+			if ((iteration+1) % config.display_iter) == 0:
+				print("Loss at iteration", iteration+1, ":", loss.item())
+			if ((iteration+1) % config.snapshot_iter) == 0:
+				
+				torch.save(dehaze_net.state_dict(), config.snapshots_folder + "Epoch" + str(epoch) + '.pth') 		
 
-	for key in list(tmp_dict.keys()):
+		# Validation Stage
+		for iter_val, (img_orig, img_haze) in enumerate(val_loader):
 
-		if key in train_keys:
-			for hazy_image in tmp_dict[key]:
+			img_orig = img_orig.cuda()
+			img_haze = img_haze.cuda()
 
-				train_list.append([orig_images_path + key, hazy_images_path + hazy_image])
+			clean_image = dehaze_net(img_haze)
 
+			torchvision.utils.save_image(torch.cat((img_haze, clean_image, img_orig),0), config.sample_output_folder+str(iter_val+1)+".jpg")
 
-		else:
-			for hazy_image in tmp_dict[key]:
+		torch.save(dehaze_net.state_dict(), config.snapshots_folder + "dehazer.pth") 
 
-				val_list.append([orig_images_path + key, hazy_images_path + hazy_image])
-
+			
 
 
-	random.shuffle(train_list)
-	random.shuffle(val_list)
 
-	return train_list, val_list
+
+
+
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+
+	# Input Parameters
+	parser.add_argument('--orig_images_path', type=str, default="/content/drive/MyDrive/PyTorch-Image-Dehazing/data/data")
+	parser.add_argument('--hazy_images_path', type=str, default="/content/drive/MyDrive/PyTorch-Image-Dehazing/data/images")
+	parser.add_argument('--lr', type=float, default=0.0001)
+	parser.add_argument('--weight_decay', type=float, default=0.0001)
+	parser.add_argument('--grad_clip_norm', type=float, default=0.1)
+	parser.add_argument('--num_epochs', type=int, default=10)
+	parser.add_argument('--train_batch_size', type=int, default=8)
+	parser.add_argument('--val_batch_size', type=int, default=8)
+	parser.add_argument('--num_workers', type=int, default=4)
+	parser.add_argument('--display_iter', type=int, default=10)
+	parser.add_argument('--snapshot_iter', type=int, default=200)
+	parser.add_argument('--snapshots_folder', type=str, default="/content/drive/MyDrive/PyTorch-Image-Dehazing/snapshots")
+	parser.add_argument('--sample_output_folder', type=str, default="/content/drive/MyDrive/PyTorch-Image-Dehazing/samples")
+
+	config = parser.parse_args(args=[])
+
+	if not os.path.exists(config.snapshots_folder):
+		os.mkdir(config.snapshots_folder)
+	if not os.path.exists(config.sample_output_folder):
+		os.mkdir(config.sample_output_folder)
+
+	train(config)
+
+
+
+
+
+
+
 
 	
-
-class dehazing_loader(data.Dataset):
-
-	def __init__(self, orig_images_path, hazy_images_path, mode='train'):
-
-		self.train_list, self.val_list = populate_train_list(orig_images_path, hazy_images_path) 
-
-		if mode == 'train':
-			self.data_list = self.train_list
-			print("Total training examples:", len(self.train_list))
-		else:
-			self.data_list = self.val_list
-			print("Total validation examples:", len(self.val_list))
-
-		
-
-	def __getitem__(self, index):
-
-		data_orig_path, data_hazy_path = self.data_list[index]
-
-		data_orig = Image.open(data_orig_path)
-		data_hazy = Image.open(data_hazy_path)
-
-		data_orig = data_orig.resize((480,640), Image.ANTIALIAS)
-		data_hazy = data_hazy.resize((480,640), Image.ANTIALIAS)
-
-		data_orig = (np.asarray(data_orig)/255.0) 
-		data_hazy = (np.asarray(data_hazy)/255.0) 
-
-		data_orig = torch.from_numpy(data_orig).float()
-		data_hazy = torch.from_numpy(data_hazy).float()
-
-		return data_orig.permute(2,0,1), data_hazy.permute(2,0,1)
-
-	def __len__(self):
-		return len(self.data_list)
-
